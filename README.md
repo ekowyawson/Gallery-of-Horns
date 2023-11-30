e63e8da6975bb31d40c832127b6036de871d6d9a
Ekow Yawson <ekowyawson71@gmail.com>
2023-11-29 20:17:53 -0500

    Completed Feature 1 and Stretch Goals for lab 3.

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 ## Task
 
+1. Import the **data.json** file into your App component and send that data into the Gallery component
+2. **Map** over the JSON data in your Gallery component to render each beast
+3. Send a function into your Gallery component that allows the user to update `state` in the App
+4. Create a `SelectedBeast` component and include it in your App
+5. Use the `state` in the App to render an individual beast in a **Modal** in the **SelectedBeast** component using *React Bootstrap*
+
+### Stretch
+
 1. Create an `input` element to allow users to **search** for an image. It is up to you to decide if the user should be able to search by title, keyword, or both.
 2. Write a **regular expression** pattern to create a **fuzzy search** so that the results are narrowed down and displayed every time the user enters or removes a character from the input.
 
@@ -11,18 +19,18 @@
 
 **Start time**: *1800*
 
-**Finish time**: *__*
+**Finish time**: *1830*
 
-**Actual time needed to complete**: *__*
+**Actual time needed to complete**: *30 minutes*
 
 ## Stretch Goal: Fuzzy search
 
 **Estimated time needed to complete**: *2 hours*
 
-**Start time**: *__*
+**Start time**: *1830*
 
-**Finish time**: *__*
+**Finish time**: *2015*
 
-**Actual time needed to complete**: *__*
+**Actual time needed to complete**: *1 hour 45 minutes*
 
 > All times are in EST
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,26 +1,53 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import { FormControl, InputGroup } from 'react-bootstrap';
+import Beast from './Beast';
 import styles from './gallery.module.css'
 
-import Beast from './Beast';
 
 function Gallery(props) {
-  return (
-    <main className={styles.main}>
-      {
-        props.list.map( (beast, index) => {
 
-          return <Beast 
-            key={index}
-            image_url={beast.image_url}
-            title={beast.title}
-            description={beast.description}
-            keyword={beast.keyword}
-          />
-          
-        })
-      }
-    </main>
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // Function to handle input change and update the search term
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  // Regular expression (regex) pattern for fuzzy search
+  const searchRegex = new RegExp(searchTerm, 'i');
+
+  // Filter the props.list based on the search term
+  const filteredList = props.list.filter(
+    (beast) =>
+      searchRegex.test(beast.title) ||
+      searchRegex.test(beast.keyword)
+  );
+
+  return (
+    <>
+      <InputGroup className="mb-3 search-bar">
+        <FormControl
+          placeholder="Search by title or keyword..."
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+      </InputGroup>
+
+      <main className={styles.main}>
+        {
+          filteredList.map( (beast, index) => (
+             <Beast 
+              key={beast._id}
+              image_url={beast.image_url}
+              title={beast.title}
+              description={beast.description}
+              horns={beast.horns}
+              keyword={beast.keyword}
+            />
+            
+          ))}
+      </main>
+    </>
   );
 }
 
